930cd037c0afb654631a4a382320e779b9f9d983
Olexandr Melnyk <me@omelnyk.net>
2015-01-14 01:59:19 +0200

    add primary keys to tables

--- sql/db-hw_comments.sql
+++ sql/db-hw_comments.sql
@@ -1,6 +1,7 @@
 -- SQL schema for HWComments
 
 CREATE TABLE `hw_comments` (
+  hw_comment_id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
   hw_user_id int unsigned NOT NULL,
   hw_page_id int unsigned NOT NULL,
   hw_timestamp CHAR(14) NOT NULL,
--- sql/db-hw_comments_count.sql
+++ sql/db-hw_comments_count.sql
@@ -1,8 +1,6 @@
 -- SQL schema for HWComments count
 
 CREATE TABLE `hw_comments_count` (
-  hw_page_id int unsigned NOT NULL,
+  hw_page_id int unsigned NOT NULL PRIMARY KEY,
   hw_comments_count int unsigned NOT NULL
 );
-
-CREATE INDEX hw_page_primary ON hw_comments_count ( hw_page_id );
